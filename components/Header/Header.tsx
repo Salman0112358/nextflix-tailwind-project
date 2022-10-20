368e59f431c63d51bfe1a52920f5097fb3cfbdb9
Salman0112358 <Salman0112358@outlook.com>
2022-10-20 18:56:47 +0100

    Additional styling for the header and a bg color change with a scroll event listener

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,30 @@
-import {StarIcon} from '@heroicons/react/24/solid'
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [scorlling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScrolling = () => {
+      window.scrollY > 0 ? setScrolling(true) : setScrolling(false);
+    };
+
+    // listening for the scrolling event then calling the handleScrolling function
+    window.addEventListener("scroll", handleScrolling);
+
+    return () => {
+      //cleaning up the event listener
+      window.removeEventListener("scroll", handleScrolling);
+    };
+  }, []);
+
   return (
-    <header>
-    {/* left div section */}
+    <header className={`${scorlling && 'bg-#160525'}`}>
+      {/* left div section */}
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
-          src="https://fontmeme.com/temporary/a41721e640ca1372bc1c424129a14b7d.png"
+          src="https://fontmeme.com/permalink/221019/f53a3ebb6987528218621b8cb9ab4017.png"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
@@ -19,9 +37,20 @@ const Header = () => {
           <li className="headerLink">My List</li>
         </ul>
       </div>
-    {/* right div section */}
-      <div>
-            <StarIcon className=""/>
+      {/* right div section */}
+      <div className="flex items-centre space-x-4 font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6 fill-white " />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6 fill-white" />
+        <Link href="/account">
+          <img
+            src="https://ih0.redbubble.net/image.618379802.1473/flat,1000x1000,075,f.u2.jpg"
+            width={24}
+            height={24}
+            alt="profile icon"
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   );
