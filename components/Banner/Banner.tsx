d5c765ad05c065f5be32b2138f83ca743be20db8
Salman0112358 <Salman0112358@outlook.com>
2022-10-20 19:19:05 +0100

    starting layout for the top featured image section  component for the app

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+const Banner = () : JSX.Element => {
+
+    return (
+        <div>
+            {/* <Image/> */}
+        </div>
+    )
+
+}
+
+export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 //---------------------Importing Components---------------------------------//
-import {Header} from '../components/index'
+import {Banner, Header} from '../components/index'
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-[200vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#0a0118] lg:h-[140vh]">
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
       <main>
-        {/* Banner */}
+          <Banner/>
         <section>{/* Row */}</section>
       </main>
       {/* Modal */}
