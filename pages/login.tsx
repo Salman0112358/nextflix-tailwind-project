e250933a903241b9c4303cf82bee1132c5ff4ca5
Salman0112358 <Salman0112358@outlook.com>
2022-10-22 01:38:16 +0100

    Adding the preview background image for the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Head from 'next/head'
+import Image from "next/image";
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className='relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
+        <Head>
+        <title>Salman's Flix</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Image
+        src="https://cdn.wallpapersafari.com/43/24/owPJtp.jpg"
+        layout='fill'
+        objectFit='cover'
+        className='-z-10 !hidden opacity-60 sm:!inline'
+        
+        />
+    </div>
   )
 }
 
